48124e19974d4064b4397849ce8a73c98ad20755
RC0033727 <RC0033727@CRO-24-13>
2019-05-14 10:48:50 +0100

    FINISHED CHANGES FOR P AND STARTED ON SPEED LIGHT

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,6 +15,36 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            int i = 3;
+            richTextBox1.AppendText(i.ToString() +"\n");
+            richTextBox1.AppendText(i.ToString() + "\n");
+
+            double pi = 3.14159265;
+            richTextBox1.AppendText(pi.ToString());
+            richTextBox1.AppendText(3.ToString());
+
+            double d1 = 5e6;
+            richTextBox1.AppendText(d1.ToString() + "\n");
+            richTextBox1.AppendText(d1.ToString("N") + "\n");
+            richTextBox1.AppendText(d1.ToString("N3") + "\n");
+            richTextBox1.AppendText(d1.ToString("N0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+            
+
+            richTextBox1.AppendText("\nTask 1\n======\n");
+            pi = 3.141592653589795;
+            double c = 299792458;
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+            richTextBox1.AppendText(c.ToString("N0") + "\n");
+            richTextBox1.AppendText(c.ToString("") + "\n");
+            richTextBox1.AppendText(c.ToString("") + "\n");
+
+
         }
     }
 }
